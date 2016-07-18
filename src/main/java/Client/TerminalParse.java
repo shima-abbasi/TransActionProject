cd97d4e65a1ac10f2374eee70a16b0eb6ddd247f
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-18 11:40:02 +0430

    Commit Num 1

---
+++ src/main/java/Client/TerminalParse.java
@@ -0,0 +1,7 @@
+package Client;
+
+/**
+ * Created by Shima Abbasi on 7/18/2016.
+ */
+public class TerminalParse {
+}
---
+++ src/main/java/Server/Deposit.java
@@ -0,0 +1,7 @@
+package Server;
+
+/**
+ * Created by Shima Abbasi on 7/18/2016.
+ */
+public class Deposit {
+}

81a1090ebc1f631a9072fa76a39ad481e3648af6
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-20 14:36:28 +0430

    Commit :D

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -2,17 +2,19 @@ package Client;
 
 import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by Shima Abbasi on 7/19/2016.
  */
 public class Client {
     public static void main(String[] args) {
-        TerminalInfo terminalInfo= null;
+        ArrayList<TransAction> transActionArray = new ArrayList<TransAction>();
+        TerminalInfo terminalInfo = null;
         TerminalParse terminalParse = new TerminalParse();
-        terminalParse.xmlParseFunction();
+        transActionArray=terminalParse.xmlParseFunction();
         try {
-            Socket socket = new Socket("localhost",Integer.parseInt(terminalParse.getServerPort()));
+            Socket socket = new Socket("localhost", Integer.parseInt(terminalParse.getServerPort()));
             DataInputStream dis = new DataInputStream(socket.getInputStream());
             String msg = dis.readUTF();
             System.out.println(msg);

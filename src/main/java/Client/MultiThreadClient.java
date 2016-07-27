b620d8cfee659e1f7c75a7bd892bca2c645302e0
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-27 17:45:54 +0430

    ServerThread added Client multi-thread added

--- src/main/java/Client/MultiThreadClient.java
+++ src/main/java/Client/MultiThreadClient.java
@@ -7,38 +7,40 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-public class MultiThreadClient {
-    static String terminal;
+public class MultiThreadClient implements Runnable {
+    String terminal;
 
     public MultiThreadClient(String terminal) {
         this.terminal = terminal;
     }
 
     public static void main(String[] args) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new MultiThreadClient("terminal0.xml");
-                new MultiThreadClient("terminal1.xml");
-                TerminalParse terminalParse = new TerminalParse(terminal);
-                terminalParse.xmlParseFunction();
-                ArrayList<Transaction> transactionArray = terminalParse.getTransactionArray();
-                try {
-                    Socket socket = new Socket(terminalParse.getServerIP(), Integer.parseInt(terminalParse.getServerPort()));
-                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-                    objectOutputStream.writeObject(transactionArray);
-                    ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                    while (true) {
-                        System.out.println(objectInputStream.readObject());
-                    }
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
+        new Thread(new MultiThreadClient("terminal0.xml")).start();
+        new Thread(new MultiThreadClient("terminal1.xml")).start();
+        new Thread(new MultiThreadClient("terminal2.xml")).start();
+
+    }
+
+    @Override
+    public void run() {
+        TerminalParse terminalParse = new TerminalParse(terminal);
+        terminalParse.xmlParseFunction();
+        ArrayList<Transaction> transactionArray = terminalParse.getTransactionArray();
+        try {
+            Socket socket = new Socket(terminalParse.getServerIP(), Integer.parseInt(terminalParse.getServerPort()));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(transactionArray);
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            while (true) {
+                System.out.println(objectInputStream.readObject());
             }
-        }).start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }

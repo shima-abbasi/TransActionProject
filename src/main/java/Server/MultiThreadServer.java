1bd47d54da4e4d13d99f173952ae321748d81a8d
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-27 12:51:14 +0430

    ServerThread added

--- src/main/java/Server/MultiThreadServer.java
+++ src/main/java/Server/MultiThreadServer.java
@@ -4,6 +4,7 @@ import Client.Transaction;
 import Server.Exceptions.InitialBalanceLimitationException;
 import Server.Exceptions.NotFoundDeposit;
 import Server.Exceptions.UpperBoundLimitationException;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -12,37 +13,63 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class MainServer {
+public class MultiThreadServer implements Runnable {
+
     static Deposit deposit = new Deposit();
     static DepositParse depositParse = new DepositParse();
     static ArrayList<Deposit> depositArray;
     ServerSocket serverSocket;
     Socket socket;
+    ObjectInputStream objectInputStream;
     ObjectOutputStream objectOutputStream;
 
+    //---------------------------------------------
+    public MultiThreadServer(Socket socket) {
+        this.socket = socket;
+    }
+    public MultiThreadServer() {
+    }
+    //----------------------------------------------
     public static void main(String[] args) {
-        MainServer mainServer = new MainServer();
+        MultiThreadServer mainServer = new MultiThreadServer();
         depositParse.jsonParserFunction();
         depositArray = depositParse.getDepositArray();
         mainServer.runServer();
     }
 
+    //--------------------------------------------------
     public void runServer() {
         try {
             serverSocket = new ServerSocket(depositParse.getServerPort());
             System.out.println("Server is up...");
-            socket = serverSocket.accept();
-            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            ArrayList<Transaction> transactions = (ArrayList<Transaction>) objectInputStream.readObject();
-            for (Transaction transaction : transactions) {
-                doTransaction(transaction);
+            while (true) {
+                socket = serverSocket.accept();
+                System.out.println("Server is connected...");
+                new Thread(new MultiThreadServer(socket)).start();
+
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
+    //------------------------------------------
+    @Override
+    public void run() {
+        try {
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            ArrayList<Transaction> transactions = (ArrayList<Transaction>) objectInputStream.readObject();
+            for (Transaction transaction : transactions) {
+                doTransaction(transaction);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
+//-----------------------------------------------
     public void doTransaction(Transaction transaction) throws IOException {
         BigDecimal newBalance = BigDecimal.ZERO;
         try {
@@ -64,4 +91,5 @@ public class MainServer {
             objectOutputStream.writeObject(e.getMessage());
         }
     }
+
 }
